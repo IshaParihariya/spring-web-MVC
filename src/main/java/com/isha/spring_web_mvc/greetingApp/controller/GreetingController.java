6ac12ec0ea5c1dc41989bb95306f85f24b9d680f
IshaParihariya <ishh1036@gmail.com>
2026-05-19 23:24:14 +0530

    Add direct controller response using HttpServletResponse

--- src/main/java/com/isha/spring_web_mvc/greetingApp/controller/GreetingController.java
+++ src/main/java/com/isha/spring_web_mvc/greetingApp/controller/GreetingController.java
@@ -2,11 +2,17 @@ package com.isha.spring_web_mvc.greetingApp.controller;
 
 
 import com.isha.spring_web_mvc.greetingApp.service.GreetingService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.net.http.HttpResponse;
 
 // controller layer
 @Controller
@@ -17,7 +23,7 @@ public class GreetingController
     @Autowired
     public GreetingService greetingService;
 
-    //bean
+    // response from contoller --> to view --> to user
     @GetMapping("/greets")
     public String getGreetings(Model model)
     //Think of Model as a bag/container to send data from Controller → JSP.
@@ -25,5 +31,14 @@ public class GreetingController
          String greetings=greetingService.getGreetings("Isha Parihariya");
          model.addAttribute("greet",greetings);
          return "greet";
+    }
+
+    // response directly from the controller --> to the user
+    @GetMapping("/wish")
+    public void getGreetings2(HttpServletResponse response) throws IOException
+    {
+        String greetings=greetingService.getGreetings("Isha Parihariya");
+        Writer writer=response.getWriter();
+        writer.write("<h1>this response is directly from the controller</h1>"+greetings);
     }
 }
